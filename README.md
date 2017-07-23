38cd62c8fa36d76ca7e463e5ba66e086d546055d
Saskia Lund <mail@saskialund.de>
2017-07-23 22:07:26 +0200

    Revert "Overall cleanout and code enhancements"
    This reverts commit 1e90cd7cad4538bd3e55d4ba4e540b25f6eb280d.

--- README.md
+++ README.md
@@ -1,7 +1,4 @@
 Divi Lightbox for Jetpack Tiled Galleries
 =========================================
 
-This plugin adds the Divi Lightbox effect to Jetpack Tiled Galleries. This is a very lightweight plugin, which has almost no impact on pageload.
-Requirements:
-+ Activated Divi theme or Divi child theme (Divi is a Premium Theme created by ElegantThemes.) 
-+ Activated Jetpack plugin with following modules enabled: Photon and Tiled Galleries
+Adds Divi's native lightbox effect to Jetpack Tiled Galleries.
--- divi-lightbox-for-jetpack-tiled-galleries.php
+++ divi-lightbox-for-jetpack-tiled-galleries.php
@@ -2,13 +2,13 @@
 
 /*
 Plugin Name: Divi Lightbox for Jetpack Tiled galleries
-Plugin URI: https://github.com/s-a-s-k-i-a/Divi-Lightbox-for-Jetpack-Tiled-Galleries/
-Description: Adds Divi's native lightbox effect to Jetpack Tiled galleries. Divi is a Premium Theme created by ElegantThemes. This plugin needs an activated Divi theme oder Divi child theme as well as activated Jetpack Plugin to make sense be functional in your WordPress installation.
+Plugin URI: https:///www.saskialund.de/wp-plugins/
+Description: Adds Divi's native lightbox effect to Jetpack Tiled galleries.
 
-Version: 1.0.1
+Version: 1.0
 
 Author: Saskia Lund
-Author URI: https://www.saskialund.de/
+Author URI: http://www.saskialund.de/
 
 License: GPLv2
 
@@ -19,58 +19,51 @@ Domain Path: /languages/
 // Prevent direct access to the plugin
 if ( !defined( 'ABSPATH' ) ) exit( 'Nice try! :)' );
 
-
 add_action( 'wp_footer', 'divi_jetpack_lightbox_js' );
-if( !function_exists( 'divi_jetpack_lightbox_js' ) ) { 
-		function divi_jetpack_lightbox_js() { 
-			if ( class_exists( "ET_Builder_Module" ) && class_exists( "Jetpack" ) ) : ?>
-			<script type="text/javascript">(function($){$(document).ready(function(){
-			if ($('.tiled-gallery').length !=0){
-				var numBilder = $('.tiled-gallery .gallery-row .gallery-group .tiled-gallery-item').length;
-				$('.tiled-gallery').addClass("et_post_gallery et_pb_gallery_items").attr("data-per_page", numBilder).wrap('<div class="et_pb_gallery"></div>');
-				$('.gallery-group.images-1').addClass("et_pb_gallery_item");
+function divi_jetpack_lightbox_js() { ?>
+<script type="text/javascript">(function($){$(document).ready(function(){
+	if ($('.tiled-gallery').length !=0){
+		var numBilder = $('.tiled-gallery .gallery-row .gallery-group .tiled-gallery-item').length;
+		$('.tiled-gallery').addClass("et_post_gallery et_pb_gallery_items").attr("data-per_page", numBilder).wrap('<div class="et_pb_gallery"></div>');
+		$('.gallery-group.images-1').addClass("et_pb_gallery_item");
 
-					if ( $('.tiled-gallery').length ) {
-						// swipe support in magnific popup only if gallery exists
-						var magnificPopup = $.magnificPopup.instance;
+			if ( $('.tiled-gallery').length ) {
+				// swipe support in magnific popup only if gallery exists
+				var magnificPopup = $.magnificPopup.instance;
 
-						$( 'body' ).on( 'swiperight', '.mfp-container', function() {
-							magnificPopup.prev();
-						} );
-						$( 'body' ).on( 'swipeleft', '.mfp-container', function() {
-							magnificPopup.next();
-						} );
-						$('.tiled-gallery .gallery-row .gallery-group').magnificPopup( {
-								delegate: '.tiled-gallery-item a',
-								type: 'image',
-								removalDelay: 500,
-								gallery: {
-									enabled: true,
-									navigateByImgClick: true
-								},
-								mainClass: 'mfp-fade',
-								zoom: {
-									enabled: ! et_pb_custom.is_builder_plugin_used,
-									duration: 500,
-									opener: function(element) {
-										return element.find('img');
-									}
-								},
-								image: {
-									titleSrc: function(item) {
-									return '<div class="mfp-title">' + item.el.find('img').attr('alt') + '</div>';
-									}
-								}
-							 });
-					}
-						
-					
+				$( 'body' ).on( 'swiperight', '.mfp-container', function() {
+					magnificPopup.prev();
+				} );
+				$( 'body' ).on( 'swipeleft', '.mfp-container', function() {
+					magnificPopup.next();
+				} );
+				$('.tiled-gallery .gallery-row .gallery-group').magnificPopup( {
+						delegate: '.tiled-gallery-item a',
+						type: 'image',
+						removalDelay: 500,
+						gallery: {
+							enabled: true,
+							navigateByImgClick: true
+						},
+						mainClass: 'mfp-fade',
+						zoom: {
+							enabled: ! et_pb_custom.is_builder_plugin_used,
+							duration: 500,
+							opener: function(element) {
+								return element.find('img');
+							}
+						},
+						image: {
+							titleSrc: function(item) {
+							return '<div class="mfp-title">' + item.el.find('img').attr('alt') + '</div>';
+							}
+						}
+					 });
 			}
-			});})(jQuery)
-		</script>
-		<?php
-		
-		endif;
-	}	
+				
+			
+	}
+	});})(jQuery)
+</script>
+<?php
 }
-
